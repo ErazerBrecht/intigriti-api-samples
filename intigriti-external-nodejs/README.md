89d89e0b584cb0203833baf9712dde6134aa7aee
Brecht Carlier <brecht@intigriti.com>
2021-05-18 20:39:51 +0200

    NodeJS > Added initial sample code

---
+++ intigriti-external-nodejs/README.md
@@ -0,0 +1,21 @@
+# intigriti-external-nodejs
+
+Sample for using intigriti api with nodejs.
+
+### Guide
+
+Get your client id & client secret from Admin > Integrations > Intigriti External API
+
+If you don't see this setting, please contact your designated intigriti success manager, they will be happy to help you.
+
+#### Installation
+
+> npm i
+
+> npm run build 
+
+> npm run start
+
+### TODO
+
+- Error handling
---
+++ intigriti-external-nodejs/src/index.ts
@@ -0,0 +1,34 @@
+import readline from 'readline-promise';
+import { Issuer } from 'openid-client';
+import got, { OptionsOfTextResponseBody } from 'got';
+
+const std = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const ask = (text: string) => std.questionAsync(text);
+const run = async () => {
+    const clientId = await ask('Client ID: ');
+    console.log(`Your provided 'Client ID': ${clientId}`)
+    const clientSecret = await ask('Client SECRET: ');
+    console.log(`Your provided 'Client SECRET': ${clientSecret}`)
+
+    const issuer = await Issuer.discover('https://login.intigriti.com');
+    const client = new issuer.Client({ client_id: clientId, client_secret: clientSecret });
+    const tokens = await client.grant({ grant_type: "client_credentials" });
+
+    const url = "https://api.intigriti.com/external/submission";
+    const options: OptionsOfTextResponseBody = { headers: { "Authorization": `Bearer ${tokens.access_token}` } };
+    const resp = await got.get(url, options);
+    console.log(JSON.stringify(JSON.parse(resp.body), null, 2));
+}
+
+run().then(() => {
+    console.log('Donzo');
+    process.exit();
+}, err => {
+    console.error('Something went wrong!');
+    console.error(err);
+    process.exit();
+});
